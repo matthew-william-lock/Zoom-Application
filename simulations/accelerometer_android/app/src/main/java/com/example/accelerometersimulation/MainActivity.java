1bffb1b5d75607d5c697c2597b6dac2ce134e3af
Matthew Lock <matthewwilliamlock@gmail.com>
2020-06-05 01:21:14 +0200

    Added real time tracking of accelerometer data and updated readme

--- simulations/accelerometer_android/app/src/main/java/com/example/accelerometersimulation/MainActivity.java
+++ simulations/accelerometer_android/app/src/main/java/com/example/accelerometersimulation/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.accelerometersimulation;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -9,18 +10,27 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.provider.ContactsContract;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
+    public static final int MAX_DATA_POINTS = 100;
     // UI Components
     TextView tv_acc_x;
     TextView tv_acc_y;
@@ -28,14 +38,27 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     TextView tv_acc_x_max;
     TextView tv_acc_y_max;
     TextView tv_acc_z_max;
-    Button bt_get_acc;
+    Button bt_acc;
 
+    // Max acc values
     private float max_acc_x=0;
     private float max_acc_y=0;
     private float max_acc_z=0;
 
+    // Sensors
     private SensorManager sensorManager;
     private Sensor sensor;
+
+    // Graph
+    private GraphView graph;
+    private LineGraphSeries<DataPoint> series_x;
+    private LineGraphSeries<DataPoint> series_y;
+    private LineGraphSeries<DataPoint> series_z;
+    private ArrayList<DataPoint> accelXList = new ArrayList<DataPoint>();
+    private ArrayList<DataPoint> accelYList = new ArrayList<DataPoint>();;
+    private ArrayList<DataPoint> accelZList = new ArrayList<DataPoint>();
+
+    private Boolean acc = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,13 +83,69 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         tv_acc_x_max=findViewById(R.id.tv_acc_x_max);
         tv_acc_y_max=findViewById(R.id.tv_acc_y_max);
         tv_acc_z_max=findViewById(R.id.tv_acc_z_max);
-        bt_get_acc = findViewById(R.id.bt_get_acc);
+        bt_acc = findViewById(R.id.bt_acc);
+
+        bt_acc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (acc) {
+                    Toast.makeText(MainActivity.this, "Stopped tracking accelerometer", Toast.LENGTH_LONG).show();
+                    acc=false;
+                } else {
+                    acc=true;
+                    Toast.makeText(MainActivity.this, "Tracking accelerometer", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
         // Accelerometer
         sensorManager=(SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensor=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
         sensorManager.registerListener(MainActivity.this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+
+        // Graph UI
+        graph = (GraphView) findViewById(R.id.graph);
+        graph.getGridLabelRenderer().setHorizontalAxisTitle("Sample (n)");
+        graph.getGridLabelRenderer().setVerticalAxisTitle("Acceleration (m/s^2)");
+        graph.setVisibility(View.VISIBLE);
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMinY(0);
+
+
+
+        series_x=new LineGraphSeries<>(accelXList.toArray(new DataPoint[0]));
+        series_y=new LineGraphSeries<>(accelYList.toArray(new DataPoint[0]));
+        series_z=new LineGraphSeries<>(accelZList.toArray(new DataPoint[0]));
+
+        // Set series legend
+        series_x.setTitle("Acceleration x");
+        series_y.setTitle("Acceleration y");
+        series_z.setTitle("Acceleration z");
+
+        // Add legend
+        graph.getLegendRenderer().setVisible(true);
+        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+
+        // Add series to graph
+        graph.addSeries( series_x);
+        graph.addSeries( series_y);
+        graph.addSeries( series_z);
+
+        // Set series colour
+        series_x.setColor(Color.RED);
+        series_y.setColor(Color.GREEN);
+        series_z.setColor(Color.BLUE);
+
+        // Set series animated props
+//        series_x.setAnimated(true);
+//        series_x.setDrawDataPoints(true);
+//        series_y.setAnimated(true);
+//        series_y.setDrawDataPoints(true);
+//        series_z.setAnimated(true);
+//        series_z.setDrawDataPoints(true);
+
 
     }
 
@@ -98,9 +177,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         float acc_y = event.values[1];
         float acc_z = event.values[2];
 
+        // Update Max values
         if (acc_x>max_acc_x) max_acc_x = acc_x;
         if (acc_y>max_acc_y) max_acc_y = acc_y;
         if (acc_z>max_acc_z) max_acc_z = acc_z;
+
+        // Add to series
+        if(acc) {
+            accelXList.add(new DataPoint(accelXList.size(), acc_x));
+            accelYList.add(new DataPoint(accelXList.size(), acc_y));
+            accelZList.add(new DataPoint(accelXList.size(), acc_z));
+            series_x.appendData(accelXList.get(accelXList.size() - 1), true, MAX_DATA_POINTS);
+            series_y.appendData(accelYList.get(accelYList.size() - 1), true, MAX_DATA_POINTS);
+            series_z.appendData(accelZList.get(accelZList.size() - 1), true, MAX_DATA_POINTS);
+        }
 
         updateUI(acc_x,acc_y,acc_z);
 
@@ -119,5 +209,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         tv_acc_x_max.setText("Acc max x : "+String.format("%2.3f",max_acc_x)) ;
         tv_acc_y_max.setText("Acc max y : "+String.format("%2.3f",max_acc_y)) ;
         tv_acc_z_max.setText("Acc max z : "+String.format("%2.3f",max_acc_z)) ;
+
+        //Update Graph
+        try {
+
+            // Set axis sizing
+            if (accelXList.size()>MAX_DATA_POINTS) graph.getViewport().setMinX(accelXList.size()-MAX_DATA_POINTS);
+            else graph.getViewport().setMinX(0);
+
+            graph.getViewport().setMaxX(accelXList.size());
+            graph.getViewport().setMaxY(Math.max(Math.max(max_acc_x,max_acc_y),max_acc_z));
+            graph.getViewport().setYAxisBoundsManual(true);
+            graph.getViewport().setXAxisBoundsManual(true);
+
+        } catch (IllegalArgumentException e) {
+            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 }
